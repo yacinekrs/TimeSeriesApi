6056a811b852c1c4c5797e78c197d803c7d0c5f9
KHORSI Yacine <yacinekhorsi1@gmail.com>
2023-11-04 23:45:50 +0100

    first commit

---
+++ src/main/java/com/TimeSeries/api/TimeSeriesapi/TimeSeriesApiApplication.java
@@ -0,0 +1,13 @@
+package com.TimeSeries.api.TimeSeriesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSeriesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSeriesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TimeSeries/api/TimeSeriesapi/TimeSeriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimeSeries.api.TimeSeriesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSeriesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
