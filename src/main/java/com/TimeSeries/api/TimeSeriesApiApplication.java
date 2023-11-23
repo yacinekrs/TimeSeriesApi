627004d1e323f5ea795fa2e0aca4313a2d055f81
rayan <rayanschanl@gmail.com>
2023-11-23 11:22:22 +0100

    first commit with DB config

--- src/main/java/com/TimeSeries/api/TimeSeriesApiApplication.java
+++ src/main/java/com/TimeSeries/api/TimeSeriesApiApplication.java
@@ -1,12 +1,14 @@
 package com.TimeSeries.api;
+import com.TimeSeries.api.model.SeriesEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication()
 public class TimeSeriesApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TimeSeriesApiApplication.class, args);
 	}
 
---
+++ src/main/java/com/TimeSeries/api/service/SeriesService.java
@@ -0,0 +1,5 @@
+package com.TimeSeries.api.service;
+
+public class SeriesService {
+
+}
