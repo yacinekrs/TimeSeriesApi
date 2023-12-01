0d0fdb30148a280d8f3c8dbbc525d7a54f3a103d
rayan <rayanschanl@gmail.com>
2023-12-01 16:17:57 +0100

    service is working with a test in the api

--- src/main/java/com/TimeSeries/api/TimeSeriesApiApplication.java
+++ src/main/java/com/TimeSeries/api/TimeSeriesApiApplication.java
@@ -1,14 +1,12 @@
 package com.TimeSeries.api;
-import com.TimeSeries.api.model.SeriesEvent;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication()
-public class TimeSeriesApiApplication {
+public class TimeSeriesApiApplication  {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(TimeSeriesApiApplication.class, args);
 	}
 
--- src/main/java/com/TimeSeries/api/repository/TimeSeriesRepository.java
+++ src/main/java/com/TimeSeries/api/repository/TimeSeriesRepository.java
@@ -1,11 +1,11 @@
 package com.TimeSeries.api.repository;
 
-import com.TimeSeries.api.model.SeriesInfo;
+import com.TimeSeries.api.model.Serie;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface TimeSeriesRepository extends CrudRepository<SeriesInfo, Long> {
+public interface TimeSeriesRepository extends CrudRepository<Serie,Long> {
 
 }
